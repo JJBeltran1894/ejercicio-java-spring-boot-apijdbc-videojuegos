1b2626015b7992ebdc3c356828a2e1ebd192ea8a
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-01 01:04:04 -0500

    JJBA: Agragadas excepciones para evitar conexion automatica de springboot a BDD - Se actualiza component scan - corregido

--- src/main/java/com/krakedev/apijdbc/ApijdbcApplication.java
+++ src/main/java/com/krakedev/apijdbc/ApijdbcApplication.java
@@ -2,8 +2,11 @@ package com.krakedev.apijdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = "com.krakedev")
 public class ApijdbcApplication {
 
 	public static void main(String[] args) {

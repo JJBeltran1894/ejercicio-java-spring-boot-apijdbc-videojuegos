a68bdb4a1c5315de33a3fa9625b8b5321231d6c2
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-01 00:58:46 -0500

    JJBA: Se agregan las clases para manejar excepciones personalizadas: ErrorResponse.java - VidejuegoDuplicadoExcepcion.java - VideojuegoNoEncontradoExcepcio.java

---
+++ src/main/java/com/krakedev/apijdbc/excepciones/VideojuegoDuplicadoExcepcion.java
@@ -0,0 +1,9 @@
+package com.krakedev.apijdbc.excepciones;
+
+public class VideojuegoDuplicadoExcepcion extends RuntimeException {
+	
+	public VideojuegoDuplicadoExcepcion(String mensaje) {
+		super(mensaje);
+	}
+
+}
---
+++ src/main/java/com/krakedev/apijdbc/excepciones/VideojuegoNoEncontradoExcepcion.java
@@ -0,0 +1,9 @@
+package com.krakedev.apijdbc.excepciones;
+
+public class VideojuegoNoEncontradoExcepcion extends RuntimeException {
+	
+	public VideojuegoNoEncontradoExcepcion(String mensaje) {
+		super(mensaje);
+	}
+
+}
